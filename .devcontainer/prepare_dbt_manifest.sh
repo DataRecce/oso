f5f4b68f75c1cce410eca1da4ccc5fa0e65802f3
Kent Huang <kent@infuseai.io>
2024-05-23 12:03:37 +0800

    [Fix] Skip dbt build when recce state file is found
    Signed-off-by: Kent Huang <kent@infuseai.io>

---
+++ .devcontainer/prepare_dbt_manifest.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# Check if the dbt's target path is existed
+if [ -d "${DIR}/../recce_state.json" ]; then
+    echo "The Recce state file is found. Skip preparing the dbt manifest."
+    exit 0
+fi
+
+echo "Preparing the dbt manifest."
+pushd ${DIR}/../
+dbt deps && dbt build && dbt docs generate
+popd
