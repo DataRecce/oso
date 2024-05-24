3ffe1cc470eb2c37f88a7a2191dd42a945ae5f6a
Kent Huang <kent@infuseai.io>
2024-05-24 15:54:16 +0800

    [Fix] Change to use github_codespace_env.sh to download artifacts
    Signed-off-by: Kent Huang <kent@infuseai.io>

--- .devcontainer/post_start_command.sh
+++ .devcontainer/post_start_command.sh
@@ -1,18 +1,9 @@
 #! /bin/bash
 
 if [ "${CODESPACES}" == "true" ]; then
-  # Set the default git repository if running in GitHub Codespaces
-  recce github artifact
-
-  # Check daily staging artifact files
-  default_branch=$(gh repo view --json defaultBranchRef --jq .defaultBranchRef.name)
-  daily_artifact_workflow_id=$(gh run list -w "${RECCE_DAILY_CI_WORKFLOW_NAME}" --status success -b dev --limit 1 --json databaseId | jq .[].databaseId)
-  gh run download $daily_artifact_workflow_id --dir .recce
-  if [ -d ".recce/dbt-artifacts" ]; then
-    mv .recce/dbt-artifacts target-base
-    echo "The daily staging artifact files are downloaded to 'target-base'."
-  fi
-
+  # Download github actions artifacts
+  bash .devcontainer/github_codespace_env.sh
+  
   # Check environment variables
   bash .devcontainer/setup_required_env.sh
 
