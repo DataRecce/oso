bc9d53004ed6b7cf4c9a9187ce6808a2750b56fc
popcorny <celu@infuseai.io>
2024-05-29 17:15:23 +0800

    Install recce nightlh in post start command
    Signed-off-by: popcorny <celu@infuseai.io>

--- .devcontainer/launch_recce_server.sh
+++ .devcontainer/launch_recce_server.sh
@@ -2,6 +2,8 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+pip install -U recce-nightly
+
 if [ -f "${DIR}/../recce_state.json" ]; then
     echo "Launching the Recce server in review mode. The Recce state file is found."
     recce server --review ${DIR}/../recce_state.json
